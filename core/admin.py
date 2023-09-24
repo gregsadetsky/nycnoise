5064e1d89f864a1278dcab3646f82eb246a0fafd
gregsadetsky <lepetitg@gmail.com>
2023-09-24 18:16:53 -0400

    base event and venue models

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 
+from .models import Event, Venue
+
 admin.site.site_title = "nyc noise"
 admin.site.site_header = "nyc noise"
+
+admin.site.register(Event)
+admin.site.register(Venue)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    name = models.CharField(max_length=255)
+    venue = models.ForeignKey("Venue", on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.name
+
+
+class Venue(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
