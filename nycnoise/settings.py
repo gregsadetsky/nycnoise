c34effd95f023dba29190ff14e141b4061fbda19
gregsadetsky <lepetitg@gmail.com>
2023-09-24 17:07:53 -0400

    do the render env dance

--- nycnoise/settings.py
+++ nycnoise/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ["ALLOWED_HOSTS"].split(",")
 
 
 # Application definition
