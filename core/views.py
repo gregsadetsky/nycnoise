6de0e4d3a842518b3b79c1a2821f0d58382e33fa
gregsadetsky <lepetitg@gmail.com>
2023-09-24 18:30:54 -0400

    basic models and template

--- core/views.py
+++ core/views.py
@@ -1,5 +1,16 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import Event, Venue
 
 
 def index(request):
-    return HttpResponse("index")
+    all_events = Event.objects.all()
+    all_venues = Venue.objects.all()
+    return render(
+        request,
+        "core/index.html",
+        {
+            "all_events": all_events,
+            "all_venues": all_venues,
+        },
+    )
