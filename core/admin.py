a5a3ee50ba4668796ac9abb2abb7a74b9e9d0734
Lucas Bezerra <42lucas.bezerra@gmail.com>
2023-10-02 12:23:01 -0700

    display description as rich text

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from .models import Event, Venue
 
@@ -7,8 +8,12 @@ admin.site.site_header = "nyc noise"
 
 
 class EventAdmin(admin.ModelAdmin):
-    list_display = ("name", "venue")
+    list_display = ("name", "venue", "get_description_as_text")
 
+    def get_description_as_text(self, obj):
+        return mark_safe(obj.description)
+    
+    get_description_as_text.short_description = 'Description'
 
 admin.site.register(Event, EventAdmin)
 
--- core/models.py
+++ core/models.py
@@ -6,7 +6,7 @@ from tinymce import models as tinymce_models
 class Event(models.Model):
     name = models.CharField(max_length=255)
     venue = models.ForeignKey("Venue", on_delete=models.PROTECT)
-    description = tinymce_models.HTMLField(max_length=1000, null = True, blank = True)
+    description = tinymce_models.HTMLField(max_length=1000, null=True, blank=True)
 
     def __str__(self):
         return self.name
