86b969db5688fcaf49b9ec886ee06bd63b2f32ab
gregsadetsky <lepetitg@gmail.com>
2023-10-14 18:04:51 -0400

    micro tweaks per conversation in #63

--- core/views.py
+++ core/views.py
@@ -1,13 +1,15 @@
-from django.shortcuts import render
-from django.db.models.functions import TruncDay
-from django.db.models import Count
 from collections import defaultdict
+
+from django.db.models.functions import TruncDay
+from django.shortcuts import render
 
 from .models import Event, Venue
 
 
 def index(request):
-    all_events = Event.objects.all().order_by("-starttime").annotate(day=TruncDay("starttime"))
+    all_events = (
+        Event.objects.all().order_by("-starttime").annotate(day=TruncDay("starttime"))
+    )
     grouped_events = defaultdict(list)
     for event in all_events:
         grouped_events[event.day].append(event)
@@ -16,6 +18,8 @@ def index(request):
         request,
         "core/index.html",
         {
+            # casting to dict since django doesn't deal with defaultdicts well
+            # https://stackoverflow.com/a/64666307
             "all_events": dict(grouped_events),
             "all_venues": all_venues,
         },
