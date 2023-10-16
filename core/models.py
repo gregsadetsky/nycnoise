9c14628dd30ce5b6e48731cdc9f89ebac867ce72
Evan Conway <evanconwaymusic@gmail.com>
2023-10-16 16:23:02 -0400

    move link generation to function

--- core/models.py
+++ core/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 from tinymce import models as tinymce_models
-from django.utils import timezone
 
 
 class Event(models.Model):
--- core/views.py
+++ core/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import render
 
 from .models import Event, Venue
 
+from .util import eventToGCalLink
+
 
 def index(request):
     all_events = (
@@ -14,22 +16,7 @@ def index(request):
     event_gcal_links = defaultdict(str)
     for event in all_events:
         grouped_events[event.day].append(event)
-        # example link
-        # https://www.google.com/calendar/render?action=TEMPLATE&text=Your+Event+Name&dates=20140127T224000Z/20140320T221500Z&details=For+details,+link+here:+http://www.example.com&location=Waldorf+Astoria,+301+Park+Ave+,+New+York,+NY+10022&sf=true&output=xml
-        event_name = event.name.replace(' ', '+')
-        print('printing event stuff')
-        print(event_name)
-        start_hour = event.starttime.hour
-        end_hour = (event.starttime.hour + 1) % 24
-        date_start = f'{event.starttime.date().isoformat().replace("-", "")}T{start_hour}{event.starttime.minute}00Z'
-        date_end = f'{event.starttime.date().isoformat().replace("-", "")}T{end_hour}{event.starttime.minute}00Z'
-        gcal_link = f'https://www.google.com/calendar/render?action=TEMPLATE'
-        gcal_link += f'&text={event_name}'
-        gcal_link += f'&dates={date_start}/{date_end}'
-        gcal_link += f'&details={event.description.replace(" ", "+")}'
-        gcal_link += f'&location={event.venue.location.replace(" ", "+")}'
-        gcal_link += '&sf=true&output=xml'
-        event_gcal_links[event.name] = gcal_link
+        event_gcal_links[event.name] = eventToGCalLink(event)
     
 
     all_venues = Venue.objects.all()
@@ -41,6 +28,6 @@ def index(request):
             # https://stackoverflow.com/a/64666307
             "all_events": dict(grouped_events),
             "all_venues": all_venues,
-            "gcal_links": dict(event_gcal_links)
+            "gcal_links": dict(event_gcal_links),
         },
     )
