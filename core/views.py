e4cdfc54d2e18906751661ea505aa78e60dbbd27
Andrea Garcia <andreagarcia749@gmail.com>
2023-10-18 15:34:33 -0400

    add a "day of" message

--- core/views.py
+++ core/views.py
@@ -3,20 +3,25 @@ from django.db.models.functions import TruncDay
 from django.db.models import Count
 from collections import defaultdict
 
-from .models import Event, Venue
+from .models import Event, Venue, DateMessage
 
 
 def index(request):
     all_events = Event.objects.all().order_by("-starttime").annotate(day=TruncDay("starttime"))
     grouped_events = defaultdict(list)
     for event in all_events:
-        grouped_events[event.day].append(event)
+        grouped_events[event.day.date()].append(event)
     all_venues = Venue.objects.all()
+    date_messages = DateMessage.objects.all()
+    date_map = {}
+    for date in date_messages:
+        date_map[date.date] = date.message
     return render(
         request,
         "core/index.html",
         {
             "all_events": dict(grouped_events),
             "all_venues": all_venues,
+            "date_messages": date_map,
         },
     )
