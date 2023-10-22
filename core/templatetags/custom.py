5ffcfc0bb9491ca0db2d8f65cedfb497a2262d9a
Evan Conway <evanconwaymusic@gmail.com>
2023-10-21 20:45:26 -0400

    move domain and url to custom tag

--- core/templatetags/custom.py
+++ core/templatetags/custom.py
@@ -11,3 +11,11 @@ def meta_title():
 def meta_description():
     return 'NYC experimental live music calendar • noise, improv, jazz, new music, avant-electronics, weirdos • performance against corporate interests!'
 
+
+@register.simple_tag
+def meta_domain():
+    return 'nyc-noise.com'
+
+@register.simple_tag
+def meta_url(url='/'):
+    return f'https://{meta_domain()}{url}'
