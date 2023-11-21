6ad3c7f6ded88f321d7347029a91e3d91d0f1736
gregsadetsky <lepetitg@gmail.com>
2023-11-20 20:31:05 -0500

    .

--- nycnoise/settings/prod.py
+++ nycnoise/settings/prod.py
@@ -1,3 +1,10 @@
 from .base import *
 
 DEBUG = False
+
+# default logging doesn't log to console with DEBUG=False
+# see https://github.com/django/django/blob/main/django/utils/log.py
+# override i.e. always log to console
+LOGGING["handlers"]["console"] = {
+    "class": "logging.StreamHandler",
+}
