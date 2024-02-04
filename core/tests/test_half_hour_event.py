bab7476be1eb1fc91253e735301f4e22c0ed4fa7
gregsadetsky <lepetitg@gmail.com>
2024-02-03 20:08:34 -0500

    update reqs; fix tests

--- core/tests/test_half_hour_event.py
+++ core/tests/test_half_hour_event.py
@@ -9,12 +9,22 @@ from django.test import TestCase
 class EventOrderingTestCase(TestCase):
     def test_that_event_at_hour_is_printed_without_minutes(self):
         nyctz = tz.gettz("America/New_York")
-        event_moment = datetime(2023, 12, 3, 19, 0, 0, 0).astimezone(nyctz)
+
+        now_datetime = datetime.now().astimezone(nyctz)
+        # set hour to 7pm, minute and seconds to 0
+        event_moment = now_datetime.replace(hour=19, minute=0, second=0)
+
+        print("event_moment", event_moment)
 
         event_title = f"event {uuid.uuid4()}"
         Event.objects.create(title=event_title, starttime=event_moment)
 
+        # getting the homepage i.e. the current/now month
         response = self.client.get("/")
+
+        with open("/tmp/response.html", "wb") as f:
+            f.write(response.content)
+
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "core/index.html")
         self.assertContains(response, event_title)
@@ -22,12 +32,18 @@ class EventOrderingTestCase(TestCase):
 
     def test_that_event_at_half_hour_is_printed_with_minutes(self):
         nyctz = tz.gettz("America/New_York")
-        event_moment = datetime(2023, 12, 3, 19, 30, 0, 0).astimezone(nyctz)
+
+        now_datetime = datetime.now().astimezone(nyctz)
+        # set hour to 7pm, minute to 30 and seconds to 0
+        event_moment = now_datetime.replace(hour=19, minute=30, second=0)
 
         event_title = f"event {uuid.uuid4()}"
         Event.objects.create(title=event_title, starttime=event_moment)
 
         response = self.client.get("/")
+
+        # print("response", response.content)
+
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "core/index.html")
         self.assertContains(response, event_title)
