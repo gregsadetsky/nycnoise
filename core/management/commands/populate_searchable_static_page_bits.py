fab0b8e328445e67c69543c6cd9970269f9cdbeb
gregsadetsky <lepetitg@gmail.com>
2024-02-17 20:07:18 -0500

    be more thorough re static page search vectors

--- core/management/commands/populate_searchable_static_page_bits.py
+++ core/management/commands/populate_searchable_static_page_bits.py
@@ -6,7 +6,10 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         # re-saving every existing static page
         # will run that page's overriden save method
-        # which will create the searchable static page bits
-        for page in StaticPage.objects.all():
+        # which will create the searchable static page bits.
+        # exceptionally run this on all_objects to be completely
+        # sure that any previously created search vectors for private static pages
+        # will be deleted. (saving a private static page deletes the related search vectors)
+        for page in StaticPage.all_objects.all():
             page.save()
         print("done")
