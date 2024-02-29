99f0eaeecd6ea8c0ae858c13daa025c4be3f61eb
Paul Feitzinger <paul@pfeyz.com>
2024-02-29 16:08:22 -0500

    test that unapproved events are hidden and the inverse scenario

--- core/tests/test_user_submitted_event.py
+++ core/tests/test_user_submitted_event.py
@@ -1,9 +1,9 @@
 import uuid
-from datetime import datetime
 from http import HTTPStatus
 
 from django.test import TestCase
 from django.urls import reverse
+from django.utils import timezone
 
 from core.models import Event, Venue
 from core.views.event_submission import UserSubmittedEventForm
@@ -22,7 +22,7 @@ class UserSubmittedEventTestCase(TestCase):
         self.assertContains(response, "<h1>Submit Event</h1>", html=True)
 
     def test_post_success(self):
-        form = UserSubmittedEventForm(dict(starttime=datetime.now(),
+        form = UserSubmittedEventForm(dict(starttime=timezone.now(),
                                            title='brutal prog matinée',
                                            artists='grand ulena',
                                            venue=self.venue.pk))
@@ -34,8 +34,33 @@ class UserSubmittedEventTestCase(TestCase):
 
         self.assertContains(response, 'event is submitted for approval')
 
+    def test_unapproved_events_are_hidden(self):
+        admin_event = Event(title=uuid.uuid4(), starttime=timezone.now())
+        admin_event.save()
+
+        unapproved_event = UserSubmittedEventForm(dict(starttime=timezone.now(),
+                                                       title=uuid.uuid4(),
+                                                       venue=self.venue.pk)).save()
+        response = self.client.get('/')
+        self.assertContains(response, admin_event.title)
+        self.assertNotContains(response, unapproved_event.title)
+
+    def test_approved_events_are_shown(self):
+        admin_event = Event(title=uuid.uuid4(), starttime=timezone.now())
+        admin_event.save()
+
+        submitted_event = UserSubmittedEventForm(dict(starttime=timezone.now(),
+                                                      title=uuid.uuid4(),
+                                                      venue=self.venue.pk)).save()
+        submitted_event.is_approved = True
+        submitted_event.save()
+
+        response = self.client.get('/')
+        self.assertContains(response, admin_event.title)
+        self.assertContains(response, submitted_event.title)
+
     def test_post_error(self):
-        form = UserSubmittedEventForm(dict(starttime=datetime.now(),
+        form = UserSubmittedEventForm(dict(starttime=timezone.now(),
                                            venue=self.venue.pk))
 
         response = self.client.post(self.endpoint, data=form.data)
@@ -48,52 +73,52 @@ class UserSubmittedEventTestCase(TestCase):
         form = UserSubmittedEventForm()
         assert not form.is_valid()
 
-        form = UserSubmittedEventForm(dict(starttime=datetime.now()))
+        form = UserSubmittedEventForm(dict(starttime=timezone.now()))
         assert not form.is_valid()
 
     def test_user_must_include_some_details(self):
         """ user must include title or artist data """
-        form = UserSubmittedEventForm(dict(starttime=datetime.now(),
+        form = UserSubmittedEventForm(dict(starttime=timezone.now(),
                                            title='brutal prog matinée',
                                            venue=self.venue))
         assert form.is_valid(), form.errors
         form.save()
 
-        form = UserSubmittedEventForm(dict(starttime=datetime.now(),
+        form = UserSubmittedEventForm(dict(starttime=timezone.now(),
                                            artists='grand ulena',
                                            venue=self.venue))
         assert form.is_valid(), form.errors
         form.save()
 
-        form = UserSubmittedEventForm(dict(starttime=datetime.now(),
+        form = UserSubmittedEventForm(dict(starttime=timezone.now(),
                                            title='brutal prog matinée',
                                            artists='grand ulena',
                                            venue=self.venue))
         assert form.is_valid(), form.errors
         form.save()
 
-        form = UserSubmittedEventForm(dict(starttime=datetime.now(),
+        form = UserSubmittedEventForm(dict(starttime=timezone.now(),
                                       venue=self.venue))
         assert not form.is_valid()
 
     def test_user_must_include_venue(self):
         """user must include venue info in either venue or venue_override
         field"""
-        form = UserSubmittedEventForm(dict(starttime=datetime.now(),
+        form = UserSubmittedEventForm(dict(starttime=timezone.now(),
                                            title='brutal prog matinée',
                                            artists='grand ulena',
                                            venue=self.venue))
         assert form.is_valid(), form.errors
         form.save()
 
-        form = UserSubmittedEventForm(dict(starttime=datetime.now(),
+        form = UserSubmittedEventForm(dict(starttime=timezone.now(),
                                            title='brutal prog matinée',
                                            artists='grand ulena',
                                            venue_override='not sure yet'))
         assert form.is_valid(), form.errors
         form.save()
 
-        form = UserSubmittedEventForm(dict(starttime=datetime.now(),
+        form = UserSubmittedEventForm(dict(starttime=timezone.now(),
                                            artists='some artists',
                                            title='some show'))
         assert not form.is_valid()
@@ -107,7 +132,7 @@ class UserSubmittedEventTestCase(TestCase):
         assert event.is_approved
 
     def test_user_submitted_events_are_unapproved(self):
-        form = UserSubmittedEventForm(dict(starttime=datetime.now(),
+        form = UserSubmittedEventForm(dict(starttime=timezone.now(),
                                            title='brutal prog matinée',
                                            artists='grand ulena',
                                            venue=self.venue))
@@ -115,7 +140,7 @@ class UserSubmittedEventTestCase(TestCase):
         assert event.user_submitted is True
         assert event.is_approved is False
 
-        form = UserSubmittedEventForm(dict(starttime=datetime.now(),
+        form = UserSubmittedEventForm(dict(starttime=timezone.now(),
                                            title='brutal prog matinée',
                                            artists='grand ulena',
                                            venue=self.venue,
