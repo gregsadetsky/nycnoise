3aff70de0255e512ff32046719f39e4e5dc010be
gregsadetsky <lepetitg@gmail.com>
2024-03-02 19:57:53 -0500

    improve sitemap; fixes #225

--- core/sitemap.py
+++ core/sitemap.py
@@ -1,15 +1,31 @@
 from django.contrib.sitemaps import Sitemap
+from django.db.models.functions import TruncMonth
 
-from .models import StaticPage
+from .models import Event, StaticPage
 
 
 class SiteSitemap(Sitemap):
     def items(self):
-        static_pages = StaticPage.objects.all()
+        url_list = ["/"]
 
-        return ["/"] + list(static_pages)
+        # all public static pages
+        for static_page in StaticPage.objects.all():
+            url_list.append(static_page.get_absolute_url())
 
+        # all approved events, extracting the year-month only,
+        # and getting a unique list of that
+        for event_year_month in set(
+            Event.objects.annotate(m=TruncMonth("starttime")).values_list(
+                "m", flat=True
+            )
+        ):
+            url_list.append(f"/{event_year_month.year}-{event_year_month.month:02d}/")
+
+        # uniquify.
+        return list(set(url_list))
+
+    # locations gets items which are supposed to be objects, and you're supposed
+    # to call get_absolute_url on those... but if you already deal with urls, you don't
+    # need to do that.
     def location(self, item):
-        if isinstance(item, StaticPage):
-            return item.get_absolute_url()
         return item
