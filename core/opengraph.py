cc9b3a047ecc3c3e4f4cbde69beede0c7435841e
k <litviakk@gmail.com>
2024-09-18 00:11:48 +0000

    Migrate Opengraph meta tags to django-meta

---
+++ core/opengraph.py
@@ -0,0 +1,15 @@
+from meta.views import Meta
+from django.templatetags.static import static
+
+
+def get_meta(url='/'):
+    return Meta(
+        title="nyc noise",
+        url=url,
+        description=(
+            "NYC experimental live music calendar • noise, improv, jazz, new music,"
+            " avant-electronics, weirdos • performance against corporate interests!"
+        ),
+        twitter_type="summary_large_image",
+        image=static("images/NYC-Noise-facebook-preview-TOO-BIG.png"),
+    )
--- nycnoise/settings/base.py
+++ nycnoise/settings/base.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     "solo",
     # https://github.com/django-ordered-model/django-ordered-model
     "ordered_model",
+    # https://github.com/nephila/django-meta
+    'meta',
 ]
 
 MIDDLEWARE = [
@@ -195,3 +197,10 @@ if PYINSTRUMENT_ENABLE:
     MIDDLEWARE += [
         "pyinstrument.middleware.ProfilerMiddleware",
     ]
+
+# Django-meta configuration
+META_USE_OG_PROPERTIES = True
+META_USE_TWITTER_PROPERTIES = True
+META_USE_TITLE_TAG = True
+META_USE_SITES = True
+META_SITE_PROTOCOL = 'https'
