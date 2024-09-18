0fe80c60bf789ffcd42870c8c1673ca04b87ce6e
k <litviakk@gmail.com>
2024-09-18 00:35:42 +0000

    Fix formatting

--- core/opengraph.py
+++ core/opengraph.py
@@ -2,11 +2,11 @@ from meta.views import Meta
 from django.templatetags.static import static
 
 
-def get_meta(url='/'):
+def get_meta(url="/"):
     return Meta(
         title="nyc noise",
         url=url,
-        object_type='website',
+        object_type="website",
         description=(
             "NYC experimental live music calendar • noise, improv, jazz, new music,"
             " avant-electronics, weirdos • performance against corporate interests!"
