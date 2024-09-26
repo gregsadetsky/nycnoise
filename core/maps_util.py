3030bf81ad8ec3066b12fad1d67c5739071c65ed
k <litviakk@gmail.com>
2024-09-26 23:00:11 +0000

    Add right user-agent and regexp to parse page

--- core/maps_util.py
+++ core/maps_util.py
@@ -1,19 +1,77 @@
 import requests
-import json
-from bs4 import BeautifulSoup
+import re
+
+reg = re.compile(
+    r"APP_INITIALIZATION_STATE=\[\[\[(-?\d*[.]\d*),(-?\d*[.]\d*),(-?\d*[.]\d*)"
+)
+
+
+def fetch_coordinates(url: str):
+    headers = {"User-Agent": "Wget/1.21"}
+
+    respone = requests.get(url, headers=headers)
+    data = respone.text
+    match = reg.search(data)
+
+    if match:
+        lat = match.group(2)
+        lon = match.group(3)
+        return (lat, lon)
+    else:
+        return (0, 0)
+
 
 def main():
-    tv_eye = "https://maps.app.goo.gl/4UtdECEnYra6kJwa8"
+
+    tv_eye = "https://maps.app.goo.gl/wgWefEqTU4VLEHAC8"
     print(f"I love tv eye: {tv_eye}")
-    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    print(fetch_coordinates(tv_eye))
+    venues_list = [
+        "https://maps.app.goo.gl/5B6wJmsggeQAQHJf8",
+        "https://maps.app.goo.gl/atAmED6wkPLdMoC38",
+        "https://maps.app.goo.gl/HypPmXet1HZvFB2KA",
+        "https://maps.app.goo.gl/86ZdyDin9rL9NdvA8",
+        "https://maps.app.goo.gl/cDJvH7NMvJ1JtoLBA",
+        "https://maps.app.goo.gl/kNcqeTHbRzs4ej8r9",
+        "https://maps.app.goo.gl/wgWefEqTU4VLEHAC8",
+        "https://goo.gl/maps/F7U6DBssH7m4ZBbD7",
+        "https://goo.gl/maps/kPbrM8ccdHBvRrQu7",
+        "https://maps.app.goo.gl/pFjgWhi7U48J1Cb27",
+        "https://maps.app.goo.gl/rV7mzttz1WA9J6dF7",
+        "https://maps.app.goo.gl/ZVc2YzAQAawSKB6m8",
+        "https://maps.app.goo.gl/VNHBMSLpkg8UJY7D7",
+        "https://maps.app.goo.gl/ai55Cqy1xY46jLEi8",
+        "https://goo.gl/maps/CJsuNbJiYcogrcie9",
+        "https://maps.app.goo.gl/V6xSxhTtUKsWCEQR8",
+        "https://goo.gl/maps/zrmoaX6oGikWZKmN9",
+        "https://goo.gl/maps/NFGUZYCsZqBhvgEU8",
+        "https://goo.gl/maps/8EbgtAhUi2HoCPA26",
+        "https://goo.gl/maps/8EbgtAhUi2HoCPA26",
+        "https://goo.gl/maps/g2vobB6yucgUdqWo6",
+        "https://goo.gl/maps/x3q4Ccc4FV9bhHkq9",
+        "https://maps.app.goo.gl/Bn2gVYMFx6Miga3c8",
+        "https://goo.gl/maps/RbGMaKKAwJ4TDVbF6",
+        "https://goo.gl/maps/HGZBPiMMX9n5EXyD8",
+        "https://maps.app.goo.gl/tQc4CBw9RAkft2Uj8",
+        "https://goo.gl/maps/atLFaJvMGEEzinFY6",
+        "https://maps.app.goo.gl/WTQLQA7h4L9AFjc7A",
+        "https://maps.app.goo.gl/TSuXLrwihAicmahq9",
+        "https://maps.app.goo.gl/Dtw5cR1c9jpbaTHJ8",
+        "https://goo.gl/maps/GSBayW2P4q7XjHok6",
+        "https://goo.gl/maps/SkQMxcLovix4nN6X9",
+        "https://maps.app.goo.gl/4YUuMmmCZPNURtFE9",
+        "https://maps.app.goo.gl/2qmpdD5U4GAuiimBA",
+        "https://goo.gl/maps/ZTscF5ufD53FKQ278",
+        "https://goo.gl/maps/euAmEEYkcAb2s1338",
+        "https://maps.app.goo.gl/j59kfWekKqxoeo8T8",
+        "https://maps.app.goo.gl/B8CLrwhgCjpzkDww7",
+        "https://goo.gl/maps/66i4iQTAG8swrQ6NA",
+        "https://goo.gl/maps/ShP3hzCMqJZ2SiE96",
+    ]
+    for v in venues_list:
+        print(f"Url: {v}")
+        print(f"Coordinates: {fetch_coordinates(v)}")
 
-    respone = requests.get(tv_eye, headers=headers)
-    print(respone.headers)
-    print(respone.status_code)
-    data = respone.text
-    # print(data)
-    soup = BeautifulSoup(data, features="lxml")
-    print(soup.prettify())
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
