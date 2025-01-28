7c9410a2ee88d4c113c7c8831d8cbd2ee942f8f0
gregsadetsky <lepetitg@gmail.com>
2025-01-27 20:45:33 -0500

    fix for date to not show minutes if its 0

--- core/views/event_redirect.py
+++ core/views/event_redirect.py
@@ -1,5 +1,6 @@
 from core.opengraph import DEFAULT_TITLE, get_meta
 from django.shortcuts import get_object_or_404, redirect, render
+from django.template.defaultfilters import date as django_date_format
 
 from ..models import Event
 from ..utils_datemath import NYCTZ
@@ -13,18 +14,22 @@ def event_redirect(request, event_id, tempate_path="core/empty.html"):
     event_month = event.starttime.strftime("%Y-%m")
 
     venue = event.venue_override if event.venue_override else event.venue
+
+    # using django's date filter below
+    # (which is typically used in templates as {{ ...|date:"format"}} )
+    # because it has the very useful "f" format specifier
+    # which is the 12-hour clock with minutes left off if they're zero
+    # i.e. '7pm', not '7:00pm'
     event_time = (
         event.starttime_override
         if event.starttime_override
-        else event.starttime.astimezone(NYCTZ).strftime("%m/%d %-I:%M %p").lower()
+        else django_date_format(event.starttime.astimezone(NYCTZ), "n/j fA").lower()
     )
     event_title = event.title_and_artists if event.title_and_artists else ""
     title = f"{event_time} @ {venue} - {event_title}"
 
     redirect_time = (
-        1
-        if request.META["HTTP_USER_AGENT"] == "TelegramBot (like TwitterBot)"
-        else 0
+        1 if request.META["HTTP_USER_AGENT"] == "TelegramBot (like TwitterBot)" else 0
     )
 
     return render(
