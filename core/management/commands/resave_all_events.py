2ba6940bb12265fd5f729f6470532cc92cdeb72b
gregsadetsky <lepetitg@gmail.com>
2025-01-27 23:27:59 -0500

    tqdm, cleanup

--- core/management/commands/resave_all_events.py
+++ core/management/commands/resave_all_events.py
@@ -1,9 +1,10 @@
 from core.models import Event
 from django.core.management.base import BaseCommand
+from tqdm import tqdm
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        for event in Event.objects.all():
+        for event in tqdm(Event.objects.all()):
             event.save()
         print("done")
